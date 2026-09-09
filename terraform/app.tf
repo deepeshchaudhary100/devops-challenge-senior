0e97167e7778b3f748dde43eb6beb7201660440f
Deepesh Chaudhary <deepeshchaudhary100@gmail.com>
2026-09-09 22:59:38 +0530

    fix: use Terraform pod template annotation to trigger rolling restart

--- terraform/app.tf
+++ terraform/app.tf
@@ -59,6 +59,9 @@ resource "kubernetes_deployment" "simpletimeservice" {
         labels = {
           app = "simpletimeservice"
         }
+        annotations = {
+          "kubectl.kubernetes.io/restartedAt" = timestamp()
+        }
       }
 
       spec {
